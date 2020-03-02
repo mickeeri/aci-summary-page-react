11d4fc31ca5228706ba94f42919b7737b55404d0
mikael-eriksson1 <mikael.eriksson1@ikea.com>
2020-03-02 15:00:38 +0100

    Update

---
+++ src/ConfirmationPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConfirmationPage = () => {
+  return (
+    <div>
+      <h1>Thank you for your payment</h1>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
--- src/CreditCardComponent.js
+++ src/CreditCardComponent.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useRef, useCallback } from "react";
-import { withRouter } from "react-router";
+import React, { useEffect, useRef, useCallback } from 'react';
+import { withRouter } from 'react-router';
 
-const CreditCardComponent = ({
-  externalSessionToken,
-  brands,
-  onBeforeSubmitCardWithAci,
-  history
-}) => {
+const CreditCardComponent = ({ externalSessionToken, onBeforeSubmitCardWithAci }) => {
   const aciScriptContainer = useRef();
 
   const initAciForm = useCallback(() => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
     script.async = true;
     script.src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${externalSessionToken}`;
 
     window.wpwlOptions = {
-      style: "plain",
-      locale: "en",
+      style: 'plain',
+      locale: 'en',
       brandDetection: true,
-      brandDetectionType: "binlist",
+      brandDetectionType: 'binlist',
       showCVVHint: true,
       onBeforeSubmitCard: onBeforeSubmitCardWithAci,
       onReadyIframeCommunication: () => {},
       onReady: () => {
-        const cardHolderInput = document.querySelector(
-          ".wpwl-control-cardHolder"
-        );
-
-        cardHolderInput.value = "Mikael Eriksson";
+        const cardHolderInput = document.querySelector('.wpwl-control-cardHolder');
+        cardHolderInput.value = 'Mikael Eriksson';
       },
-      useSummaryPage: true,
-      onSaveTransactionData: () => {
-        history.push("/summary-page");
-      }
     };
 
     aciScriptContainer.current.appendChild(script);
-  }, [externalSessionToken, onBeforeSubmitCardWithAci, history]);
+  }, [externalSessionToken, onBeforeSubmitCardWithAci]);
 
   useEffect(() => {
     initAciForm();
@@ -45,12 +33,12 @@ const CreditCardComponent = ({
 
   return (
     <div
-      style={{ marginTop: "50px" }}
+      style={{ marginTop: '50px' }}
       className="CreditCardComponent my-l-2xs"
       data-testid="CreditCardComponent"
     >
       <form
-        action="https://docs.oppwa.com/tutorials/integration-guide/advanced-options"
+        action={`${window.location.origin}/confirmation-page/`}
         className="paymentWidgets"
         data-brands="VISA MASTER AMEX"
       />
