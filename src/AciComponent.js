ce35c079b0ddb81a48d538391dd9d78bc10ee1e4
mikael-eriksson1 <mikael.eriksson1@ikea.com>
2020-03-02 15:59:01 +0100

    Rename to AciComponent

--- src/AciComponent.js
+++ src/AciComponent.js
@@ -1,46 +1,35 @@
 import React, { useEffect, useRef } from 'react';
 import { withRouter } from 'react-router';
 
-const AciComponent = ({ checkoutId, onBeforeSubmitCardWithAci }) => {
+const AciComponent = ({ externalSessionToken }) => {
   const aciScriptContainer = useRef();
 
   useEffect(() => {
-    function initAciForm() {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${checkoutId}`;
-      script.id = 'aci-script';
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${externalSessionToken}`;
 
-      if (document.getElementById('aci-script')) return;
+    window.wpwlOptions = {
+      style: 'plain',
+      locale: 'en',
+      inlineFlow: ['KLARNA_PAYMENTS_PAYLATER'],
+      onReady: () => {
+        // Nothing happens here unless wrapping in a setTimeout.
+        window.wpwl.executePayment('wpwl-container-virtualAccount-KLARNA_PAYMENTS_PAYLATER');
+      },
+    };
 
-      window.wpwlOptions = {
-        style: 'plain',
-        locale: 'en',
-        inlineFlow: ['KLARNA_PAYMENTS_PAYLATER'],
-        onReady: () => {
-          window.wpwl.executePayment('wpwl-container-virtualAccount-KLARNA_PAYMENTS_PAYLATER');
-        },
-      };
-
-      aciScriptContainer.current.appendChild(script);
-    }
-
-    if (checkoutId) initAciForm();
-  }, [checkoutId]);
-
-  if (!checkoutId) {
-    return <p>Please submit a Checkout ID</p>;
-  }
+    aciScriptContainer.current.appendChild(script);
+  }, [externalSessionToken]);
 
   return (
-    <div style={{ marginTop: '5rem' }} className="AciComponent my-l-2xs" data-testid="AciComponent">
+    <div style={{ marginTop: '50px' }}>
       <form
         action={`${window.location.origin}/confirmation-page/`}
         className="paymentWidgets"
         data-brands="KLARNA_PAYMENTS_PAYLATER"
       />
-
       <div ref={aciScriptContainer} />
     </div>
   );
