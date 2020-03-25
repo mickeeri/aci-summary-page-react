d27d4ad5c72bbc5c250e6be45f145e024307c689
mikael-eriksson1 <mikael.eriksson1@ingka.com>
2020-03-25 13:23:19 +0100

    Fix Klarna inline flow

--- src/AciComponent.js
+++ src/AciComponent.js
@@ -1,35 +1,46 @@
 import React, { useEffect, useRef } from 'react';
 import { withRouter } from 'react-router';
 
-const AciComponent = ({ externalSessionToken }) => {
+const AciComponent = ({ checkoutId, onBeforeSubmitCardWithAci }) => {
   const aciScriptContainer = useRef();
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${externalSessionToken}`;
+    function initAciForm() {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${checkoutId}`;
+      script.id = 'aci-script';
 
-    window.wpwlOptions = {
-      style: 'plain',
-      locale: 'en',
-      inlineFlow: ['KLARNA_PAYMENTS_PAYLATER'],
-      onReady: () => {
-        // Nothing happens here unless wrapping in a setTimeout.
-        window.wpwl.executePayment('wpwl-container-virtualAccount-KLARNA_PAYMENTS_PAYLATER');
-      },
-    };
+      if (document.getElementById('aci-script')) return;
 
-    aciScriptContainer.current.appendChild(script);
-  }, [externalSessionToken]);
+      window.wpwlOptions = {
+        style: 'plain',
+        locale: 'en',
+        inlineFlow: ['KLARNA_PAYMENTS_PAYLATER'],
+        onReady: () => {
+          window.wpwl.executePayment('wpwl-container-virtualAccount-KLARNA_PAYMENTS_PAYLATER');
+        },
+      };
+
+      aciScriptContainer.current.appendChild(script);
+    }
+
+    if (checkoutId) initAciForm();
+  }, [checkoutId]);
+
+  if (!checkoutId) {
+    return <p>Please submit a Checkout ID</p>;
+  }
 
   return (
-    <div style={{ marginTop: '50px' }}>
+    <div style={{ marginTop: '5rem' }} className="AciComponent my-l-2xs" data-testid="AciComponent">
       <form
         action={`${window.location.origin}/confirmation-page/`}
         className="paymentWidgets"
         data-brands="KLARNA_PAYMENTS_PAYLATER"
       />
+
       <div ref={aciScriptContainer} />
     </div>
   );
