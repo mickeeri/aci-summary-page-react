c98ed3e27aee85dc2a34405788f24dadb6acbe4a
mikael-eriksson1 <mikael.eriksson1@ingka.com>
2021-05-04 15:18:32 +0200

    Updates

---
+++ precache-manifest.e4b1917e589cd529ed16c61d0c1d2a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cef73d84aa67935c505808fd4b491c9",
+    "url": "/aci-minimal-react-app/index.html"
+  },
+  {
+    "revision": "922d914ecc8f9825b4d6",
+    "url": "/aci-minimal-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e48413ffd755fa013f1c",
+    "url": "/aci-minimal-react-app/static/js/2.17e3172b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/aci-minimal-react-app/static/js/2.17e3172b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "922d914ecc8f9825b4d6",
+    "url": "/aci-minimal-react-app/static/js/main.75f1d340.chunk.js"
+  },
+  {
+    "revision": "e1c4e14bc0efc8e4dbb1",
+    "url": "/aci-minimal-react-app/static/js/runtime-main.c0bb681d.js"
+  }
+]);
